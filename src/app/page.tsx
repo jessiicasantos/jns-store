f7718c73ebe3358fc8152ccc9404a6f02e080a03
email <jessiicansantos@gmail.com>
2023-10-20 14:49:27 -0300

    Add button, card and header components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div></div>;
+  return <div>Home</div>;
 }
 
 // ctrl+shift+p > Format Document With... > Configure Default Formatter... > Prettier - Code formatter
