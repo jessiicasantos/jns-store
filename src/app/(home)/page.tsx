c2f39ac603bae22c2e0dedef2ea245f00269619f
email <jessiicansantos@gmail.com>
2023-10-25 19:48:45 -0300

    Add second banner to home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -27,8 +27,18 @@ const Home = async () => {
       </div>
 
       <div className="mt-8">
+        <p className="mb-2 pl-5 font-bold uppercase">Ofertas</p>
         <ProductList products={deals} />
       </div>
+
+      <Image
+        src="/banner-home-02.png"
+        height={0}
+        width={0}
+        className="h-auto w-full px-5"
+        sizes="100vw"
+        alt="Até 50% de desconto esse mês!"
+      />
     </div>
   );
 };
