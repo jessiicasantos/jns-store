eadfd639c23922660a77cf1a1e8247bbf4f20b91
email <jessiicansantos@gmail.com>
2023-10-25 20:51:32 -0300

    Add footer component

---
+++ src/components/ui/footer/footer.tsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="text=[0.625rem] bg-accent px-8 py-4 opacity-75">
+      © 2023 Copyright FSW Store
+    </div>
+  );
+};
+
+export default Footer;
